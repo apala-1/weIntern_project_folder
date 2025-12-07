a95613779a710416f75b3721e72e776744f247b4
apala-1 <apalalamichhane13579@gmail.com>
2025-12-07 10:26:24 +0545

    Function to add the students to the file

--- student_crud.py
+++ student_crud.py
@@ -14,3 +14,19 @@ def save_data():
     with open(filename, "w") as f:
         json.dump(students, f, indent=4)
 
+def add_student():
+    print("Add student here: ")
+    name = input("Name: ")
+    age = input("Age: ")
+    grade = input("Grade: ")
+
+    student = {
+        "id": len(students) + 1,
+        "name": name,
+        "age": age,
+        "grade": grade,
+    }
+
+    students.append(student)
+    save_data()
+    print("The student is added.")
