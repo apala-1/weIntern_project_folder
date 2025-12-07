3c9a7b8f7b9b1e1b5c81f084e4551e37a980d24f
apala-1 <apalalamichhane13579@gmail.com>
2025-12-07 10:34:34 +0545

    Validation code added

--- student_crud.py
+++ student_crud.py
@@ -14,6 +14,21 @@ def save_data():
     with open(filename, "w") as f:
         json.dump(students, f, indent=4)
 
+def get_valid_int(value):
+    """Forces the user to input a valid integer."""
+    val = input(value)
+    if(val.isdigit):
+        return int(val)
+    print("Please return a valid number.")
+
+def get_non_empty(value):
+    """Forces the user to input non-empty text."""
+    while True:
+        val = input(value).strip
+        if val:
+            return val
+        print("This field cannot be empty.")
+
 def add_student():
     print("Add student here: ")
     name = input("Name: ")
